8f7f95d7f6ee6ba77e4da0f79431f59a2d58860d
cowenowner <scowen@email.arizona.edu>
2022-12-24 05:43:22 -0700

    Update rotary_encoder.ino

--- src/rotary_encoder/rotary_encoder.ino
+++ src/rotary_encoder/rotary_encoder.ino
@@ -1,77 +1,117 @@
 ///////////////////////////////////////////////////////
-// Code to sense a rotary encoder.
-// LPD3806 – Optical Rotary Encoder
+// Code to sense a rotary encoder and send signals out the serial and digital outputs.
 //
+// LPD3806 – Optical Rotary Encoder
+// If you use a different encoder, you should just need to adjust the two TICS 
+// constants for your particular encoder and it should work.
+//
+// The serial out is formatted for a csv file that has the... 
+//    time(ms), direction, tic, full rotation.
 ///////////////////////////////////////////////////////
-// Cowen and also code from 
+// Cowen 2022 and also help from
 // https://www.instructables.com/Tutorial-of-Rotary-Encoder-With-Arduino/
 // https://www.circuitschools.com/rotary-encoder-with-arduino-in-detail-with-example-codes/
 ///////////////////////////////////////////////////////
 
-#define encoder0PinA 2
-#define encoder0PinB 3
-#define outPinTic 4
-#define outPinDirection 5
-#define TTL_DELAY_MS 2
-#define TICS_PER_SIGNAL 100
+#define encoderPinA 2 // Needs to be the interrupt pin on the Arduino
+#define encoderPinB 3 // Second signal line from encoder
+#define outPinTic 4 // Sends a signal for each Tic
+#define outPinDirection 5 // 1 if CW, 0 if CCW
+#define outPinFullTic 6 // Sends a pulse for each full rotation.
+#define TTL_DELAY_MS 2 // Duration the TTL is high. Be sure you data acquisition system can handle the delay.
+#define TICS_PER_SIGNAL 100 // Send a signal every xx Tics. Many acquisition systems have a hard time keeping up with the full tic rate (up to 5000 tics/second) so we only send a signal every xx tics.
+#define TICS_PER_ROTATION 600 // Tics for a full 360degrees rotation.
 
-int encoderPos = 0;
+long encoderPos = 0;
 int tempEncoderPos = 0;
+int tempEncoderPosFullRot = 0;
 int ticCountCW = 0;
 int ticCountCCW = 0;
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(encoder0PinA, INPUT_PULLUP);
-  pinMode(encoder0PinB, INPUT_PULLUP);
+  Serial.begin(115200); // Be sure your com port is set to receive this. The high rate helps if spinning fast perhaps.
+  pinMode(encoderPinA, INPUT_PULLUP);
+  pinMode(encoderPinB, INPUT_PULLUP);
   pinMode(outPinTic, OUTPUT);
   pinMode(outPinDirection, OUTPUT);
-  digitalWrite(outPinTic,0);
-  digitalWrite(outPinDirection,0);
+  digitalWrite(outPinTic, 0);
+  digitalWrite(outPinDirection, 0);
   attachInterrupt(0, doEncoder, RISING); // Must be rising
 }
-int lastValRotary;
-int valRotary;
+
+long lastValRotary;
 int send_signal = 0;
+int send_full_rot_signal = 0;
+unsigned long currentMillis = 0;
+
 void loop() {
-  if (send_signal > 0 ){
+  
+  if (send_signal > 0 ) {
+    currentMillis = millis();
     send_signal = 0;
-    digitalWrite(outPinTic,HIGH);
-    delay(TTL_DELAY_MS);
-    digitalWrite(outPinTic,LOW);
-    if(valRotary>lastValRotary) {
+    digitalWrite(outPinTic, HIGH);
+    Serial.print(currentMillis);
+    
+    if (encoderPos > lastValRotary) {
       digitalWrite(outPinDirection, HIGH);
-      Serial.print(" CW ");
-      // Serial.print(ticCountCW);
-    }
-    if(valRotary<lastValRotary)  {
+      Serial.print(",1");
+    }else if (encoderPos < lastValRotary)  {
       digitalWrite(outPinDirection, LOW);
-      Serial.print(" CCW ");
-      //Serial.print(ticCountCCW);
+      Serial.print(",-1");
+    }else {
+      // This should rarely if ever happen.
+      Serial.print(",0");
     }
-    Serial.print(" re ");
-    Serial.print(valRotary);
-    Serial.println(" ");
-  }
-  lastValRotary = valRotary;
-}
-void doEncoder()
-{ // Only runs on rising edge   
-    if (digitalRead(encoder0PinA) != digitalRead(encoder0PinB))
-    {
-      encoderPos++;
-      tempEncoderPos++;
-      //ticCountCW++;
-      //ticCountCCW = 0;
+    lastValRotary = encoderPos;
+    
+    delay(TTL_DELAY_MS); // A possible alternative would be to do this asynchronously.
+    digitalWrite(outPinTic, LOW); // ensures the direction signal and tic overlap.
+    
+    Serial.print(",");
+    Serial.print(encoderPos);
+
+    if (send_full_rot_signal == 1) {
+      send_full_rot_signal = 0;
+      digitalWrite(outPinFullTic, HIGH);
+      delay(TTL_DELAY_MS);
+      digitalWrite(outPinFullTic, LOW);
+      Serial.print(",1");
     }else{
-      encoderPos--;
-      tempEncoderPos--;
-      // ticCountCCW++;
-      // ticCountCW = 0;
+      Serial.print(",0");
     }
-    valRotary = encoderPos;
-    if (abs(tempEncoderPos) >= TICS_PER_SIGNAL){
-      tempEncoderPos = 0;
-      send_signal = 1;
-    }
+    
+    Serial.println();
+
+  }
+
+}
+
+void doEncoder()
+{ // Only runs on rising edge
+
+  if (digitalRead(encoderPinA) != digitalRead(encoderPinB))
+  {
+    encoderPos++;
+    tempEncoderPos++;
+    tempEncoderPosFullRot++;
+    //ticCountCW++;
+    //ticCountCCW = 0;
+  } else {
+    encoderPos--;
+    tempEncoderPos--;
+    tempEncoderPosFullRot--;
+    // ticCountCCW++;
+    // ticCountCW = 0;
+  }
+
+  if (abs(tempEncoderPos) >= TICS_PER_SIGNAL) {
+    tempEncoderPos = 0;
+    send_signal = 1;
+  }
+
+  if (abs(tempEncoderPosFullRot) >= TICS_PER_ROTATION) {
+    tempEncoderPosFullRot = 0;
+    send_full_rot_signal = 1;
+  }
+
 }
